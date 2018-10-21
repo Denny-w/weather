4884d349c69b21bd3a9ddfd500d85367a1eb8dd7
Ernest <wangyw.zt@gmail.com>
2018-10-21 10:06:22 +0000

    Apply fixes from StyleCI

--- src/Exceptions/Exception.php
+++ src/Exceptions/Exception.php
@@ -3,13 +3,11 @@
  * Created by PhpStorm.
  * User: Clown
  * Date: 18/10/21
- * Time: 14:14
+ * Time: 14:14.
  */
 
 namespace ErnestWang\Weather\Exceptions;
 
-
 class Exception extends \Exception
 {
-
 }
--- src/Exceptions/HttpException.php
+++ src/Exceptions/HttpException.php
@@ -3,13 +3,11 @@
  * Created by PhpStorm.
  * User: Clown
  * Date: 18/10/21
- * Time: 14:16
+ * Time: 14:16.
  */
 
 namespace ErnestWang\Weather\Exceptions;
 
-
 class HttpException extends Exception
 {
-
 }
